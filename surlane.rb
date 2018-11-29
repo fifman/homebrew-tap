5f3b2e38093c742052f9b3cae096d49a7bfe762e
Yicheng Feng <1264380449@qq.com>
2018-11-29 11:28:21 +0800

    fix

--- surlane.rb
+++ surlane.rb
@@ -18,21 +18,20 @@ class Surlane < Formula
 
   def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>Label</key>
-    <string>#{plist_name}</string>
-    <key>ProgramArguments</key>
-    <array>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
         <string>#{opt_bin}/surlane</string>
-        <string>-c /etc/surlane/config.toml</string>
-    </array>
-    <key>KeepAlive</key>
-    <true/>
-</dict>
-</plist>
-
+        <string>-c=/etc/surlane/config.toml</string>
+        </array>
+        <key>KeepAlive</key>
+        <true/>
+    </dict>
+    </plist>
   EOS
   end
 
