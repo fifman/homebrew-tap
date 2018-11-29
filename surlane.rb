de7a780206614210245ef719950fe591023f6314
fifman <1264380449@qq.com>
2018-11-29 17:50:53 +0800

    Brew formula update for surlane version v0.2.0

--- surlane.rb
+++ surlane.rb
@@ -1,16 +1,17 @@
 class Surlane < Formula
   desc "secure tunnel like shadowsocks, but lightweight"
   homepage ""
-  url "https://github.com/fifman/surlane/releases/download/v0.1.0/surlane_0.1.0_Darwin_x86_64.tar.gz"
-  version "0.1.0"
-  sha256 "7a600bd1162467d52d00a6f6389dd08f63cb4f33f748045bb9aa989d1a21147c"
+  url "https://github.com/fifman/surlane/releases/download/v0.2.0/surlane_0.2.0_Darwin_x86_64.tar.gz"
+  version "0.2.0"
+  sha256 "485b5040b46fb9b030403d1f191480ad76e63c2b624a349fdfbe3fac52e89436"
 
   def install
     bin.install "surlane"
+    (etc/"surlane").install "config.toml"
   end
 
   def caveats; <<~EOS
-    secure tunnel like shadowsocks, but lightweight
+    config file is stored in #{etc}/surlane/config.toml
   EOS
   end
 
@@ -18,20 +19,25 @@ class Surlane < Formula
 
   def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>#{plist_name}</string>
+    <key>ProgramArguments</key>
+    <array>
         <string>#{opt_bin}/surlane</string>
-        <string>-c=/etc/surlane/config.toml</string>
-        </array>
-        <key>KeepAlive</key>
-        <true/>
-    </dict>
-    </plist>
+        <string>-c=#{etc}/surlane/config.toml</string>
+    </array>
+    <key>KeepAlive</key>
+    <true/>
+    <key>StandardErrorPath</key>
+    <string>#{var}/log/surlane.log</string>
+    <key>StandardOutPath</key>
+    <string>#{var}/log/surlane.log</string>
+</dict>
+</plist>
+
   EOS
   end
 
