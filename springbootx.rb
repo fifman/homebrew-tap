3ae11b80730ac61998dacc6e8db303072352502e
Yicheng Feng <1264380449@qq.com>
2019-02-14 09:50:48 +0800

    revise

---
+++ springbootx.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class Springbootx < Formula
+  homepage 'https://spring.io/projects/spring-boot'
+  url 'https://github.com/fifman/spring-boot/blob/master/spring-boot-project/spring-boot-cli/release/spring-boot-cli-2.2.0.BUILD-SNAPSHOT-bin.tar.gz'
+  version '2.2.0.BUILD-SNAPSHOT'
+  sha256 '2d02229a4a083b98452c31e8563f2b29d55c628d65694f21bc26c365a9e63a75'
+  head 'https://github.com/fifman/spring-boot.git'
+
+  if build.head?
+    depends_on 'maven' => :build
+  end
+
+  def install
+    if build.head?
+      Dir.chdir('spring-boot-projects/spring-boot-cli') { system 'mvn -U -DskipTests=true package' }
+      root = 'spring-boot-projects/spring-boot-cli/target/spring-boot-cli-*-bin/spring-*'
+    else
+      root = '.'
+    end
+
+    bin.install Dir["#{root}/bin/spring"]
+    lib.install Dir["#{root}/lib/spring-boot-cli-*.jar"]
+    bash_completion.install Dir["#{root}/shell-completion/bash/spring"]
+    zsh_completion.install Dir["#{root}/shell-completion/zsh/_spring"]
+  end
+end
