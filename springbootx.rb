c46dd0b95f8602c8ddd6e232e4b733b33df639da
Yicheng Feng <1264380449@qq.com>
2019-02-18 11:09:19 +0800

    new springbootx

--- springbootx.rb
+++ springbootx.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Springbootx < Formula
   homepage 'https://spring.io/projects/spring-boot'
-  url 'https://github.com/fifman/spring-boot/releases/download/2.2.0.BUILD-SNAPSHOT/spring-boot-cli-2.2.0.BUILD-SNAPSHOT-bin.tar.gz'
-  version '2.2.0.BUILD-SNAPSHOT'
-  sha256 '9903be949a85eb4264dd6dae3eb5147c0b6201d981dee1f1e09f0785fc039956'
+  url 'https://github.com/fifman/springcli/releases/download/1.0.0/springcli-1.0.0-bin.tar.gz'
+  version '1.0.0'
+  sha256 'ca2aa4a98d55e5a40fa590fc5dee60285cd13581128a0818a2c27c6cd4fb5cdb'
   head 'https://github.com/fifman/spring-boot.git'
 
   if build.head?
@@ -20,7 +20,7 @@ class Springbootx < Formula
     end
 
     bin.install Dir["#{root}/bin/spring"]
-    lib.install Dir["#{root}/lib/spring-boot-cli-*.jar"]
+    lib.install Dir["#{root}/lib/springcli-*.jar"]
     bash_completion.install Dir["#{root}/shell-completion/bash/spring"]
     zsh_completion.install Dir["#{root}/shell-completion/zsh/_spring"]
   end
